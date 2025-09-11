84d0aa2e080cb449d6bb6c55467ba436520c765d
Juan Rhenals <Juandavidrhenalsmontero@gmail.com>
2025-09-11 10:04:48 -0500

    Cambios requeridos
    Cambios requeridos

--- formulario/lib/bloc/home_state.dart
+++ formulario/lib/bloc/home_state.dart
@@ -9,10 +9,23 @@ sealed class HomeState extends Equatable {
 
 final class HomeInitial extends HomeState {}
 
-final class HomeSucess extends HomeState {}
+final class HomeSucess extends HomeState {
+  final String nombre;
+  final String correo;
+  final String password;
+
+  const HomeSucess({
+    required this.nombre,
+    required this.correo,
+    required this.password,
+});
+}
 
 final class HomeLoading extends HomeState {}
 
 final class HomeFailure extends HomeState {
-  
+  final String mensaje;
+  const HomeFailure({
+    required this.mensaje
+  });
 }
---
+++ formulario/lib/modelo/modelo.dart
@@ -0,0 +1,8 @@
+class Usuario {
+
+  final String? nombre;
+  final String correo;
+  final String password;
+  
+const Usuario({this.nombre, required this.correo, required this.password, });
+}
